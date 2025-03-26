9e74cd558e88364cea5c63c87394555f64185639
Nancy Quinonez <nancyq29@hotmail.com>
2025-03-26 15:15:12 -0500

    added css, finished assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
 import { useState } from 'react'
 import './App.css'
 import {puppyList} from './data.js'
+import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
 
- console.log(puppies); 
+  const [featPupId, setFeatPupId] = useState(null);
+  
+  // console.log(puppies); 
+  
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+    console.log(featuredPup);
 
   return (
     <div className = 'App'>
+
       {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p onClick={()=>{
+              setFeatPupId(puppy.id)
+              console.log(puppy.id)
+            }} key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })
       }
+
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}  
     </div>
   );
+
 }
 
 export default App
